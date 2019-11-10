71a5e5ac4fc4ce1d2f98c11be060e96a8a737f8c
Soumya Veer <veer.soumya@gmail.com>
2019-11-09 17:53:17 -0800

    Create NavBar Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,19 @@
-import React , {Component} from 'react';
-
+import React, { Component } from 'react';
+import { Navbar, Nav } from "react-bootstrap";
 
 class NavBar extends Component {
 
-  render(){
+  render() {
     return (
-      <div>
-        NavBar
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">New Question</Nav.Link>
+            <Nav.Link href="#link">LeaderBoard</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
