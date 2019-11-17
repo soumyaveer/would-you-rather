b8b3835e43a92d1b19c4e7a86d8784b0d75fbd55
Soumya Veer <veer.soumya@gmail.com>
2019-11-17 14:57:22 -0800

    Update App.js to redirect to users home page.
    Move getting and filtering of users to App component from Login Component
    Refactor code to make App a class component instead of functional component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ class NavBar extends Component {
       <Navbar bg="light" expand="lg" >
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-sm-auto">
-            <Nav.Link href="/"> Home </Nav.Link>
+            <Nav.Link href="/home"> Home </Nav.Link>
             <Nav.Link href="/create_question">New Question</Nav.Link>
             <Nav.Link href="/leaderboard">LeaderBoard</Nav.Link>
           </Nav>
