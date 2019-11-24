ad1aa6186916a0938021b4da916cd5248afcc17a
Soumya Veer <veer.soumya@gmail.com>
2019-11-24 15:30:13 -0800

    Add LeaderBoard component

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class LeaderBoard extends Component {
+  render() {
+
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default LeaderBoard;
