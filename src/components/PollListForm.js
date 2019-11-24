b262c4b95b26e2bae049c856cabfeaefd3ce52b5
Soumya Veer <veer.soumya@gmail.com>
2019-11-24 12:58:54 -0800

    Fix to allow user to select only one option.
    Cause: The input for radio button was set such that it did not capture the option selected by the user.
    Fix: Add state to the PollListItemForm and update the state whenever user changes the slection. This allowed dynamic selectiuon and capture of the options of radio button.

--- src/components/PollListForm.js
+++ src/components/PollListForm.js
@@ -3,37 +3,69 @@ import { Card, InputGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class PollListForm extends Component {
+  state = {
+    question: {},
+    selectedOption: ''
+  };
+
+  componentDidMount() {
+    const { question } = this.props;
+    this.setState({
+      question
+    })
+  }
+
+  handleRadioButtonSelectionChange = (event) => {
+    console.log(event.target.value);
+    const selectedOption = event.target.value;
+    this.setState({
+      selectedOption
+    });
+  }
+
   render() {
     const { question } = this.props;
     console.log("This is what I am receiving", question)
     return (
-      <div>
+      <form className='form-group'>
         <Card bg="light" className="text-center">
           <Card.Header>
             <h3>{question.author} asks</h3>
           </Card.Header>
           <Card.Body>
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Radio aria-label="Radio"/>
-              </InputGroup.Prepend>
-              {question.optionOne.text}
-              OR
-              <InputGroup.Prepend>
-                <InputGroup.Radio aria-label="Radio" />
-              </InputGroup.Prepend>
-              {question.optionTwo.text}
-            </InputGroup>
+            <div className='Radio'>
+              <label>
+                <input
+                  type="radio"
+                  name="answer"
+                  value="optionOne"
+                  checked={this.state.selectedOption === 'optionOne'}
+                  onChange={this.handleRadioButtonSelectionChange}/>
+                {question.optionOne.text}
+              </label>
+            </div>
+
+            <div className='Radio'>
+              <label>
+                <input
+                  type="radio"
+                  name="answer"
+                  value="optionTwo"
+                  checked={this.state.selectedOption === 'optionTwo'}
+                  onChange={this.handleRadioButtonSelectionChange}/>
+                {question.optionTwo.text}
+              </label>
+            </div>
 
             <Link
               to={`/polls/${question.id}`}
               className='btn btn-outline-info'
             >
-              View Poll
+              Submit
             </Link>
           </Card.Body>
         </Card>
-      </div>
+      </form>
     )
   }
 }
