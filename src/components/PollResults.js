fab427f295caf5266804ac696cac3fd1407c7f62
Soumya Veer <veer.soumya@gmail.com>
2019-11-25 12:16:56 -0800

    Add PollResults component

---
+++ src/components/PollResults.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class PollResults extends Component {
+  render(){
+    return(
+      <div>
+        PollResults
+      </div>
+    )
+  }
+}
+
+export default PollResults;
