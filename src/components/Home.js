67eb02937108d55553dbd8ede3436afc1d494f3c
Soumya Veer <veer.soumya@gmail.com>
2019-11-29 13:21:19 -0800

    Revert back to class Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuestionsList from "./QuestionsList";
 import { connect } from 'react-redux';
 
-const Home = (props) => {
-  const handleOnPollSelect = (value) => {
+class Home extends Component {
+  handleOnPollSelect = (value) => {
     this.props.onPollSelect(value)
   }
 
-  const { unansweredQuestions, answeredQuestions, questions, authedUser } = props;
-  return (
-    <QuestionsList
-      className='well text-center'
-      questions={questions}
-      unansweredQuestions={unansweredQuestions}
-      answeredQuestions={answeredQuestions}
-      currentUser={authedUser}
-      onPollSelect={handleOnPollSelect}
-    />
-  )
+  render() {
+    const { unansweredQuestions, answeredQuestions, questions, authedUser } = this.props
+    return (
+      <QuestionsList
+        className='well text-center'
+        questions={questions}
+        unansweredQuestions={unansweredQuestions}
+        answeredQuestions={answeredQuestions}
+        currentUser={authedUser}
+        onPollSelect={this.handleOnPollSelect}
+      />
+    )
+  }
 }
+
 
 const mapStateToProps = ({ questions, authedUser, users }) => {
   const questionsArray = Object.values(questions);
-
   const answeredQuestions = questionsArray.filter(question =>
     question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser)
   );
@@ -38,6 +40,6 @@ const mapStateToProps = ({ questions, authedUser, users }) => {
     answeredQuestions,
     unansweredQuestions
   }
-};
+}
 
 export default connect(mapStateToProps)(Home);
