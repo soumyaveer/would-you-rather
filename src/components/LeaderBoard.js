6f8444e0fbd9d9abfdd45074a3419ed57039b536
Soumya Veer <veer.soumya@gmail.com>
2019-11-29 12:30:19 -0800

    Refactor leaderboard to remove hardcoded values and replace thmem with dynamic values

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
 import { Card, Image } from "react-bootstrap";
+import { connect } from 'react-redux';
 
 class LeaderBoard extends Component {
-  state = {
-    scoreCards: [
-      {
-        "name": "Sarah Edo",
-        "avatar": "https://api.adorable.io/avatars/150/5ed5ebf51885555f88b3d9dcad4694a4.png",
-        "numAnswers": 4,
-        "numQuestions": 2
-      },
-      {
-        "name": "John Doe",
-        "avatar": "https://api.adorable.io/avatars/150/e41ea4b7bf72eebb86b95f501994def5.png",
-        "numAnswers": 3,
-        "numQuestions": 2
-      },
-      {
-        "name": "Tyler McGinnis",
-        "avatar": "https://api.adorable.io/avatars/150/dbc98435fafe77aa6c310558b1d5a17b.png",
-        "numAnswers": 2,
-        "numQuestions": 2
-      }
-    ]
-  };
-
   render() {
-    const { scoreCards } = this.state;
+    const { scoreCards } = this.props;
     return (
       <div className='container'>
-        <ul className='list-unstyled' style={{marginTop: 10}}>
+        <ul className='list-unstyled' style={{ marginTop: 10 }}>
           {
             scoreCards.map(scoreCard =>
               <li key={scoreCard.name} className='mx-auto'>
@@ -46,7 +24,7 @@ class LeaderBoard extends Component {
                     </div>
                     <h5>Answered Questions: {scoreCard.numAnswers}</h5>
                     <h5>Created Questions: {scoreCard.numQuestions}</h5>
-                    <h5> Score: {scoreCard.numAnswers + scoreCard.numQuestions}</h5>
+                    <h5> Score: {scoreCard.totalPolls}</h5>
                   </Card.Body>
                 </Card>
                 <br/>
@@ -59,4 +37,20 @@ class LeaderBoard extends Component {
   }
 }
 
-export default LeaderBoard;
+const mapStateToProps = ({ users }) => {
+  const allUsers = Object.values(users)
+  const scoreCards = allUsers.map(user => ({
+      id: user.id,
+      name: user.name,
+      avatar: user.avatarURL,
+      numAnswers: Object.values(user.answers).length,
+      numQuestions: user.questions.length,
+      totalPolls: Object.values(user.answers).length +  user.questions.length
+    })).sort((a, b) => b.totalPolls - a.totalPolls)
+
+  return {
+    scoreCards
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
